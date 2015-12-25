bfae65f0291568cdc1d47acf40dfa497c88e6968
Brian Tai <alphabt@gmail.com>
2015-12-25 15:07:08 +0800

    Added Notices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'notices#index'
+  resources :notices
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
